4fec902522445c465506c6ff7ea41b6879253ba8
nnayak4 <nirushanagaraj_nayak@intuit.com>
2024-04-13 02:37:36 +0530

    feat: update

--- Array/BestTimeToBuyAndSellStocks2.py
+++ Array/BestTimeToBuyAndSellStocks2.py
@@ -57,5 +57,13 @@ class Solution:
         profit=profit+localProfit
         return profit
 
+## simple logic wow!
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit=0
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                profit+= prices[i]-prices[i-1]
+        return profit
             
 
