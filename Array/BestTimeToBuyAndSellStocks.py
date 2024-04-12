f0b20f2e5d0de9a7ec0166ed7bb990a4096ffcd4
nnayak4 <nirushanagaraj_nayak@intuit.com>
2024-04-13 01:23:29 +0530

    feat: updtecode

--- Array/BestTimeToBuyAndSellStocks.py
+++ Array/BestTimeToBuyAndSellStocks.py
@@ -34,3 +34,19 @@ class Solution:
         for i in range(len(prices)):
             Profit=max(Profit, maxPos[i]-prices[i])
         return Profit
+
+
+## sliding window method, uses O(1) space
+# intution is buy when low, sell when high
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+       l,r=0,1 #left=buy, right=sell
+       profit=0
+       while(r<len(prices)):
+            #profitable?
+            if prices[l]<prices[r]:
+                profit=max(profit, prices[r]-prices[l])
+            else:
+                l=r
+            r+=1
+       return profit
