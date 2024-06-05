2871a0981674db6b320fdb11ff95888b75ac1f34
nnayak4 <nirushanagaraj_nayak@intuit.com>
2024-06-05 13:25:04 +0530

    feat: leetcode day 16

--- Tree/BinarySearch/270. Closest Binary Search Tree Value.py
+++ Tree/BinarySearch/270. Closest Binary Search Tree Value.py
@@ -19,3 +19,32 @@
 # 0 <= Node.val <= 109
 # -109 <= target <= 109
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        result=[float("inf"),0]
+        def dfs(node):
+            if not node:
+                return
+            if abs(node.val-target)<=result[0]:
+                if abs(node.val-target)==result[0]:
+                    if node.val<result[1]:
+                        result[1]=node.val
+                else:
+                    result[1]=node.val
+                result[0]=abs(node.val-target)
+
+            if node.val<=target:
+                return dfs(node.right)
+            else:
+                return dfs(node.left)
+        dfs(root)
+        return result[1]
+
+
+        
